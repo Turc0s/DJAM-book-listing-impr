74b6d48e94b7e3d00345897a92f353a94c7845c3
tapsa <tapsa872@gmail.com>
2018-02-18 21:03:00 +0200

    Added the edit

--- mean-book-listing-impr/src/app/book-edit/book-edit.component.ts
+++ mean-book-listing-impr/src/app/book-edit/book-edit.component.ts
@@ -13,49 +13,22 @@ import { Book } from '../book.model';
 })
 export class BookEditComponent implements OnInit {
 
-  // constructor(private router: Router, private route: ActivatedRoute,
-  //             private _bookControlService: BookControlService) { }
-
-  // ngOnInit() {
-  //   this.getBookEditDetails(this.route.snapshot.params["id"]);
-  // }
-
-  // getBookEditDetails(id) {
-  //   this._bookControlService.getSingleBook(id)
-  //   .subscribe(data => {
-  //     this._bookControlService.book = data;
-  //   });
-  // }
-
-  // updateBook(id) {
-  //  this._bookControlService.updateBook(id)
-  //     .subscribe(res => {
-  //         let id = res['_id'];
-  //         this.router.navigate(['/book-details', id]);
-  //       }, (err) => {
-  //         console.log(err);
-  //       }
-  //     );
-  // }
-
-  book: any = {};
-
-  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute,
+              private _bookControlService: BookControlService) { }
 
   ngOnInit() {
-    console.log("nginit book-edit getBook");
-    this.getBook(this.route.snapshot.params['id']);
+    this.getBookEditDetails(this.route.snapshot.params["id"]);
   }
 
-  getBook(id) {
-    this.http.get('/book/'+id).subscribe(data => {
-      this.book = data;
+  getBookEditDetails(id) {
+    this._bookControlService.getSingleBook(id)
+    .subscribe(data => {
+      this._bookControlService.book = data;
     });
   }
 
   updateBook(id) {
-    this.book.updated_date = Date.now();
-    this.http.put('/book/'+id, this.book)
+   this._bookControlService.updateBook(id)
       .subscribe(res => {
           let id = res['_id'];
           this.router.navigate(['/book-details', id]);
@@ -64,5 +37,32 @@ export class BookEditComponent implements OnInit {
         }
       );
   }
+
+  // book: any = {};
+
+  // constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
+
+  // ngOnInit() {
+  //   console.log("nginit book-edit getBook");
+  //   this.getBook(this.route.snapshot.params['id']);
+  // }
+
+  // getBook(id) {
+  //   this.http.get('/book/'+id).subscribe(data => {
+  //     this.book = data;
+  //   });
+  // }
+
+  // updateBook(id) {
+  //   this.book.updated_date = Date.now();
+  //   this.http.put('/book/'+id, this.book)
+  //     .subscribe(res => {
+  //         let id = res['_id'];
+  //         this.router.navigate(['/book-details', id]);
+  //       }, (err) => {
+  //         console.log(err);
+  //       }
+  //     );
+  // }
 
 }
